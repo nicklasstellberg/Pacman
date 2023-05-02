3e238fc8670a9be0c779b4e4a55879eaea9cb8c8
nicklasstellberg <nicklas.stellberg@hotmail.com>
2023-05-02 15:12:21 +0300

    Reset highscore

---
+++ Assets/Scripts/DeletePlayerPrefs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletePlayerPrefs : MonoBehaviour
+{
+    // Tämä skripti liitetään GameObjectiin, joten sen täytyy periytyä MonoBehaviour-luokasta.
+
+    // Update-funktio kutsutaan jokaisen pelin päivityksen yhteydessä.
+    void Update()
+    {
+        // Tarkistetaan, onko pelaaja painanut näppäintä "M".
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            // Jos pelaaja on painanut näppäintä "M", poistetaan kaikki tallennetut tiedot.
+            PlayerPrefs.DeleteAll();
+        }
+    }
+
+    public void resetHighScore()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
+
